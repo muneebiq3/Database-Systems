edfb40db75fd3d371f0416a8aca3c8f8f6bc073c
muneebiq3 <muneebiq526@gmail.com>
2024-05-07 02:36:10 +0500

    Initial Commit

---
+++ Practice/Practice.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS `Practice`;
+USE `Practice`;
+CREATE TABLE EMPLOYEE
+(
+	employee_id numeric primary key,
+    first_name varchar(20),
+    last_name varchar(20),
+    email varchar(30),
+    position varchar(50),
+    salary numeric (10,2)
+);
+describe employee;
+
+create table new_employee as
+select employee_id, first_name
+from employee;
+
+use `new_employee`;
+alter table `new_employee` drop column `employee_id`;
+create schema if not exists `bank`;
+use `bank`;
+drop schema `bank`;
+use `Practice`;
+ALTER TABLE `EMPLOYEE` modify COLUMN `first_name` VARCHAR(20) NOT NULL;
+use `PRACTICE`;
